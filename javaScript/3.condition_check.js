abebcbbab86168ac5807012fe298a23d0331abea
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-05 02:33:25 +0530

    condition check negative and positive number check and more than number check

---
+++ javaScript/3.condition_check.js
@@ -0,0 +1,31 @@
+
+// Check even / odd
+let nums = 21;
+
+if (nums % 2 === 0) {
+    console.log("even number");
+} else {
+    console.log("Odd number");
+}
+
+
+// Check even / odd
+let nums1 = 0;
+if (nums1 > 0) {
+    console.log("This is positive number");
+} else if (nums1 < 0) {
+    console.log("This is negative number");
+} else {
+    console.log("This is zero")
+}
+
+
+// Check number > 100
+let nums2 = 100;
+if (nums2 > 100) {
+    console.log("This is number more than 100");
+} else {
+    console.log("This is number less than or equal to 100");
+}
+
+
