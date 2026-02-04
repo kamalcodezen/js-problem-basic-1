3421294291eef1f3f1dd72efcf4a66b903df6183
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-04 20:22:26 +0530

    print 1 -10 increment and print decrement 10-1

---
+++ javaScript/1.print.js
@@ -0,0 +1,33 @@
+// print 1-5
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+console.log(`-------------------`);
+
+
+// Print 1–10
+let i = 0;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+console.log(`-------------------`);
+
+// Print 5–1
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+
+console.log(`-------------------`);
+
+
+// Print 10–1
+let n = 10;
+while (n >= 1) {
+    console.log(n);
+    n--;
+}
+
+console.log(`-------------------`);
